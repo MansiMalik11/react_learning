67a515f58747bbc569915ca5a5e5aef82e76861c
Mansi <srdevphp@gmail.com>
2025-09-09 18:01:35 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header,{Fruit, Color, User} from "./Header"
+
+function App(){
+  return (
+    <div>
+      <Header/>
+      <Fruit />
+      <Color />
+      <h1>hello world</h1>
+      <h1>Mansi learning React</h1>
+      <h3>{User}</h3>
+      <h3>{10*2}</h3>
+      <button onClick={()=>alert("Mansi Malik")}>Click</button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,27 @@
+function Header(){
+    return(
+        <div>
+            <h1>header is under construc..</h1>
+            <h2>Enjoy the learning</h2>
+        </div>
+    )
+}
+
+export const User = "$$";
+
+export function Fruit(){
+    return(
+        <div className="class">
+        <h1>fruit are tasty</h1>
+        </div>
+    )
+}
+
+export function Color(){
+    return(
+        <div className="class">
+        <h1>Color is red</h1>
+        </div>
+    )
+}
+export default Header
