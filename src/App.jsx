19799d94c3667e4c314cde3f0dcaddf253f890fb
Mansi <mansimalik890@gmail.com>
2025-09-18 18:27:20 +0530

    Learn: function normalfun Clickevents funcalling variableusein jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import Task from "./Task"
 import Header, {Fruit, Color, User} from "./Header"
-
+ //outside function will work without an error if inside function got commented
+ const arrowfun=()=>{
+    alert("This is outside arrow function");
+  }
 function App(){
+  //Normal function
+  function callme(){
+    alert("This is normal function");
+  }
+  //Arrow function
+  const arrowfun=()=>{
+    alert("This is arrow function");
+  }
+  //by passing parameters 
+  function param(name){
+    alert(name);
+  }
   return (
     <div>
       <Header/>
@@ -13,6 +28,12 @@ function App(){
       <h3>{User}</h3>
       <h3>{10*2}</h3>
       <button onClick={()=>alert("Mansi Malik")}>Click</button>
+      <button onClick={callme}>Click</button>
+      <button onClick={arrowfun}>Arrow Function</button>
+      <button onClick={()=>param("Mansi")}>Parameter pass Function</button>
+      <button onClick={()=>param("Malik")}>Parameter pass Function</button>
+
+
     </div>
   )
 }
