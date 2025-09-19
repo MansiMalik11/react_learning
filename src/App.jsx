b4da7858536c72a04d1ceccee2496ce8cf5e7564
Mansi <mansimalik890@gmail.com>
2025-09-19 13:37:57 +0530

    Learn: Toggle or hide/show of element and component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from "react";
 import Counter from "./Counter";
-function App()
-{
-const [fruit,setFruit]=useState("Mango");
-  function handleFruit(){
-    setFruit("Grapes");
-  }
+function App() {
+  const [display, setDisplay] = useState(true);
+
   return (
-    <div>  
-      <h1>State in React</h1>
-      <h2>{fruit}</h2>
-      <button onClick={handleFruit}>Change fruit</button>
-      <Counter></Counter>
+    <div>
+      <h1>Toggle or Hide and show in React</h1>
+      <button onClick={() => setDisplay(!display)}>Toggle</button>
+      {display ? <h2>Mansi</h2> : null}
+      {display ? <Counter /> : null}
+
     </div>
   )
 }
