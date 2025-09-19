7e6168dcde9cb5b293bfa86b2e3f83d783802765
Mansi <mansimalik890@gmail.com>
2025-09-19 18:56:29 +0530

    Learn: else if Conditions OR cases

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -12,6 +12,12 @@ const Counter = () => {
             <h2>SimpleCounter: {number}</h2>
             <h2>Reverse Counter: {rCounter}</h2>
             <button onClick={() => { setNumber(number + 1) }}>Simple number</button>
+            {/* //condition apply else if Or make cass  */}
+            {
+                number==1?<h1>Condition 1</h1>
+                :number==3?<h1>Condition 3</h1>
+                :<h1> Other Condition </h1>
+            }
             <button onClick={() => { setRCounter(rCounter - 1) }}>Reverse number</button>
         </div>
     )
